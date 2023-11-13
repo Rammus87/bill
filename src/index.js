dac23997680b772ffc0a3b92f7fc9adf5d05a806
Rammus87 <131853744+Rammus87@users.noreply.github.com>
2023-11-13 12:36:24 +0800

    Tabbar and Route

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import sun from '@/test'
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
+import { Provider } from 'react-redux';
+import store from './store';
+
+import './theme.css';
 
 console.log(sun(2,2))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <RouterProvider router={router} />
+    </Provider>
 );
 
 
